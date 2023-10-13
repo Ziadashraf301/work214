b8cffcf1190b129879e1eac3b9cda3c240c1edf0
Ziad Ashraf <111798631+Ziadashraf301@users.noreply.github.com>
2023-10-13 09:41:08 +0300

    Add files via upload

---
+++ spam_detection.py
@@ -0,0 +1,26 @@
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+import sys
+
+# Define the pipeline
+pipeline = Pipeline([
+    ('tfidf', TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)),
+    ('model', LogisticRegression())
+])
+
+# Load the saved model
+pipeline = joblib.load('spam_model.pkl')
+
+# Example usage
+if len(sys.argv) > 1:
+    input_mail = [sys.argv[1]]
+    prediction = pipeline.predict(input_mail)
+
+    if prediction == 1:
+        print("This is a Ham Mail.")
+    else:
+        print("This is a Spam Mail.")
+else:
+    print("Please provide text input.")
